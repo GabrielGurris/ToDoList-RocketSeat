7da2b67a2e9f71f7859862d9637ec28c6b880d2a
gabrielgurris <gabriel_gurris@outlook.com>
2023-08-08 22:49:03 -0300

    changes in tasks

--- src/Task.jsx
+++ src/Task.jsx
@@ -11,9 +11,15 @@ export function Task(){
                     className={styles.input}
                 />
                 <a href="#" className={styles.a}>Criar <img src={logoPlus} alt="" /></a>
-                
             </form>
-            
+            <header className={styles.header}>
+                <span>Tarefas criadas
+                    <div className={styles.number}>0</div>
+                </span>
+                <span>Concluídas
+                    <div className={styles.number}>0</div>
+                </span>
+            </header>
         </div>
     )
 }
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -4,18 +4,10 @@ import styles from './TasksList.module.css';
 export function TasksList() {
     return (
         <div>
-            <header className={styles.header}>
-                <span>Tarefas criadas
-                    <div className={styles.p}>0</div>
-                </span>
-                <span>Concluídas
-                    <div className={styles.p}>0</div>
-                </span>
-            </header>
             <div className={styles.main}>
                 <div className={styles.round}>
                     <input type="checkbox" id="checkbox" />
-                    <label for="checkbox"></label>
+                    <label htmlFor="checkbox"></label>
                 </div>
                 <span>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</span>
                 <img src={trash} alt="" />
